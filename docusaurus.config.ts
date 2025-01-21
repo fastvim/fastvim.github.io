74801086c33e2f423b314df0497fbfe6a30b3a92
BrunoCiccarino <landneco0@gmail.com>
2025-01-21 19:47:54 -0300

    fix(): config fixxed

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,23 +9,15 @@ const config: Config = {
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://fastvim.github.io',
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'fastvim', // Usually your repo name.
+  organizationName: 'fastvim', 
+  projectName: 'fastvim', 
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -37,8 +29,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/fastvim/fastvim.github.io/',
         },
@@ -50,8 +40,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+ //   image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'documentation',
       logo: {
